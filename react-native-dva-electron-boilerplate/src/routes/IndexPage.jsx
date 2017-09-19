886484575e19f7b01da873f3412967f124680e6d
iamjiyixuan <iamjiyixuan@gmail.com>
2017-09-19 10:39:45 +0800

    use antd

--- react-native-dva-electron-boilerplate/src/routes/IndexPage.jsx
+++ react-native-dva-electron-boilerplate/src/routes/IndexPage.jsx
@@ -1,31 +1,51 @@
 import React from 'react';
 import { connect } from 'dva';
+
 import styles from './IndexPage.css';
 
-function IndexPage(props) {
-  console.log('IndexPage props', props);
+import { Layout, Button, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Footer, Content, Sider } = Layout;
+
+
+function IndexPage({ dispatch, count }) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <hr />
-      <h2>计数器</h2>
-      <button onClick={() => {
-        props.dispatch({ type: 'count/add' });
-      }}>+</button>
-      <button onClick={() => {
-        props.dispatch({ type: 'count/addDelay' });
-      }}>*+</button>
-      <button onClick={() => {
-        props.dispatch({ type: 'count/minus' });
-      }}>-</button>
-      {props.count}
-      <hr />
-      <h2>gank.io api</h2>
-      <button onClick={() => {
-        props.dispatch({ type: 'gank/getGankData' });
-      }}>api/data/Android/10/1</button>
-      <hr />
+    <div>
+      <Layout>
+        <Header style={{ background: 'white', borderStyle: 'solid', borderWidth: 0, borderColor: 'lightgray' }}>
+          Yay! Welcome to dva!
+        </Header>
+        <Layout>
+          <Sider width={200} style={{ background: 'white' }}>
+            侧边栏
+          </Sider>
+          <Layout>
+            <Content style={{ background: 'white', borderStyle: 'solid', borderWidth: 1, borderColor: 'lightgray', minHeight: 280, padding: 10 }}>
+
+              <h2>计数器</h2>
+              <Button style={{ marginRight: 10, }} size='small' onClick={() => {
+                dispatch({ type: 'count/add' });
+              }}>+</Button>
+              <Button style={{ marginRight: 10, }} size='small' onClick={() => {
+                dispatch({ type: 'count/addDelay' });
+              }}>*+</Button>
+              <Button style={{ marginRight: 10, }} size='small' onClick={() => {
+                dispatch({ type: 'count/minus' });
+              }}>-</Button>
+              count:{count}
+
+              <h2>gank.io api</h2>
+              <Button style={{ marginRight: 10, }} size='small' onClick={() => {
+                dispatch({ type: 'gank/getGankData' });
+              }}>api/data/Android/10/1</Button>
+            </Content>
+          </Layout>
+        </Layout>
+
+        <Footer style={{ background: 'white', borderStyle: 'solid', borderWidth: 0, borderColor: 'lightgray' }}>
+          Footer
+        </Footer>
+      </Layout>
     </div>
   );
 }
